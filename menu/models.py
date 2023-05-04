240e41b77f02a8bb08293a59c126765d3f5e85a4
coderharshmalik <harshmalik20015@gmail.com>
2023-05-05 00:15:39 +0530

    5

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# item
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField()
+
+    # category
+
+# category
+# name
