4c3eb9e42129eb8f04f08d28d1ab58927a624f34
coderharshmalik <harshmalik20015@gmail.com>
2023-05-07 14:51:53 +0530

    6

--- menu/models.py
+++ menu/models.py
@@ -3,9 +3,20 @@ from django.db import models
 # item
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField()
-
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    #style = models.CharField(max_length=60)
+    #number = models.IntegerField()
     # category
 
 # category
 # name
+
+class category(models.Model):
+    choice=[
+        ("br","breakfast"),
+        ("lu","lunch"),
+        ("sn","snacks"),
+        ("dn","dinner")
+    ]
+    name = models.CharField(max_length=120)#name of dish
+    category = models.CharField(max_length=2,choices=choice)
